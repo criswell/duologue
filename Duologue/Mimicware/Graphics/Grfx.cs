c929db70e8d8031c8d6ffe5add077606bd5b34ef
Sam Hart <criswellious@gmail.com>
2008-09-22 19:42:32 -0400

    Add stubbed graphic effects class

---
+++ Duologue/Mimicware/Graphics/Grfx.cs
@@ -0,0 +1,86 @@
+#region File Description
+#endregion
+
+#region Using statements
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Graphics;
+using Mimicware.Manager;
+using Mimicware.Debug;
+#endregion
+
+namespace Mimicware.Graphics
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class Grfx : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+        #region Constants
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor / Init / Load
+        public Grfx(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+        #endregion
+
+        #region Update / Draw
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+        #region Public Methods
+        #endregion
+    }
+}
