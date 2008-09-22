5f3d7fbe67cc09eb715fd400d9d3fd7e37429f9e
Sam Hart (criswellious <Sam Hart (criswellious@gmail.com / Sam@stark)>
2008-09-22 19:21:31 -0400

    Add standard level state

---
+++ Duologue/State/StandardLevelState.cs
@@ -0,0 +1,30 @@
+#region File Description
+#endregion
+
+#region Using statements
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Graphics;
+using Mimicware.Manager;
+using Mimicware.Debug;
+// Duologue
+using Duologue.PlayObjects;
+using Duologue.State;
+#endregion
+
+namespace Duologue.State
+{
+    class StandardLevelState
+    {
+    }
+}
