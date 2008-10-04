4f97f1775b3f82b839c552a6c139dd1dcdd42a81
Sam Hart <criswellious@gmail.com>
2008-10-04 11:24:43 -0400

    Add a game play screen test

---
+++ Duologue/Tests/GamePlayScreenTest.cs
@@ -0,0 +1,34 @@
+#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware.Manager;
+using Mimicware.Graphics;
+// Duologue
+using Duologue;
+using Duologue.Properties;
+using Duologue.Screens;
+#endregion
+
+namespace Duologue.Tests
+{
+    class GamePlayScreenTest : GameScreen
+    {
+        protected override void InitializeConstants()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
