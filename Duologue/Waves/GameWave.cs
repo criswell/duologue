d9fa0b54aba23ec49e1c7ab893a2a320fa60a733
Sam Hart <criswellious@gmail.com>
2008-10-05 02:38:51 -0400

    more work fleshing out gamewave

--- Duologue/Waves/GameWave.cs
+++ Duologue/Waves/GameWave.cs
@@ -24,6 +24,11 @@ using Duologue.State;
 
 namespace Duologue.Waves
 {
+    public enum PlayerShotType
+    {
+        Default,
+    }
+
     public class GameWave
     {
         #region Fields
@@ -34,6 +39,38 @@ namespace Duologue.Waves
         /// The name of the wave
         /// </summary>
         public string Name;
+
+        /// <summary>
+        /// The list of wavelets associated with this game wave
+        /// </summary>
+        public Wavelet[] Wavelets;
+
+        /// <summary>
+        /// The list of possible player shop types in this GameWave
+        /// </summary>
+        public PlayerShotType[] PossiblePlayerShotTypes;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default, empty constructor
+        /// </summary>
+        public GameWave()
+        {
+        }
+
+        /// <summary>
+        /// Constructs a Game Wave object
+        /// </summary>
+        /// <param name="name">The name of this wave</param>
+        /// <param name="numWavelets">The maximum number of wavelets associated with this wave</param>
+        /// <param name="numShotTypes">The maximum number of shots associated with this wave</param>
+        public GameWave(string name, int numWavelets, int numShotTypes)
+        {
+            Name = name;
+            Wavelets = new Wavelet[numWavelets];
+            PossiblePlayerShotTypes = new PlayerShotType[numShotTypes];
+        }
         #endregion
     }
 }
