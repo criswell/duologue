9c57248f705fc7f4d58a41256fa475c0ed87d7b3
gsmith <gsmith@mimicware.net>
2008-10-24 16:23:10 -0400

    Removed GamePlayScreenTest from game assembly project

--- Duologue.Test/MainMenuTest.cs
+++ Duologue.Test/MainMenuTest.cs
@@ -1,6 +1,18 @@
+//System
 using System;
 using System.Collections.Generic;
 using System.Text;
+//Xna
+using Microsoft.Xna.Framework;
+//Mimicware
+//Duologue
+using Duologue;
+using Duologue.PlayObjects;
+using Duologue.UI;
+using Duologue.Waves;
+//MbUnit
+using MbUnit.Framework;
+
 
 namespace Duologue.Test
 {
