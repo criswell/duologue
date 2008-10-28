28954da0d7759a39c2e5a4275eca3d2f4d6b711f
Sam Hart <criswellious@gmail.com>
2008-10-28 14:39:09 -0400

    Add comments for additional blit layers

--- Duologue/DuologueGame.cs
+++ Duologue/DuologueGame.cs
@@ -36,12 +36,13 @@ namespace Duologue
     /// BLIT LAYERS
     /// -----------
     /// 1.0f        -       Background
+    ///             -       Beam, shot items
     /// 0.9f
     /// 0.8f        -       Spinner (default)
     /// 0.7f
     /// 0.6f
-    /// 0.5f
-    /// 0.4f
+    /// 0.5f        -       Players (default)
+    /// 0.4f        -       Players (additional elements, tracks, etc)
     /// 0.3f
     /// 0.2f
     /// 0.1f
