f63766ae7b57fb875dc17cf804ae48ab78743343
Sam Hart <criswellious@gmail.com>
2008-11-08 10:26:59 -0500

    Add new Enemy_Buzzsaw class, stubbed out

---
+++ Duologue/PlayObjects/Enemy_Buzzsaw.cs
@@ -0,0 +1,68 @@
+#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Graphics;
+using Mimicware.Manager;
+// Duologue
+using Duologue.State;
+#endregion
+
+namespace Duologue.PlayObjects
+{
+    class Enemy_Buzzsaw : PlayObject
+    {
+        #region Constants
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructor / Init / Load
+        #endregion
+
+        #region Private methods
+        #endregion
+
+        #region Public methods
+        #endregion
+
+        #region Draw / Update
+        #endregion
+
+        #region Public overrides
+        public override bool StartOffset()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override bool UpdateOffset(PlayObject pobj)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override bool ApplyOffset()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override bool TriggerHit(PlayObject pobj)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+        #endregion
+    }
+}
