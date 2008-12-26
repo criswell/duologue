963b4dd07e8837c8653a6da1009d6663a1173912
Sam Hart <criswellious@gmail.com>
2008-12-25 20:41:26 -0500

    Much work on an explosion system for the player

---
+++ Duologue/ParticleEffects/PlayerSmoke.cs
@@ -0,0 +1,67 @@
+﻿#region File Description
+#endregion
+
+#region Using statements
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Graphics;
+using Mimicware.Manager;
+using Mimicware.Debug;
+using Mimicware.Fx;
+#endregion
+
+namespace Duologue.ParticleEffects
+{
+    class PlayerSmoke : ParticleSystem
+    {
+        public PlayerSmoke(Game game, int howManyEffects)
+            : base(game, howManyEffects)
+        {
+        }
+
+        protected override void InitializeConstants()
+        {
+            textureFilename = "player-smoke";
+
+            // less initial speed than the explosion itself
+            minInitialSpeed = 20;
+            maxInitialSpeed = 200;
+
+            // acceleration is negative, so particles will accelerate away from the
+            // initial velocity.  this will make them slow down, as if from wind
+            // resistance. we want the smoke to linger a bit and feel wispy, though,
+            // so we don't stop them completely like we do ExplosionParticleSystem
+            // particles.
+            minAcceleration = -10;
+            maxAcceleration = -50;
+
+            // explosion smoke lasts for longer than the explosion itself, but not
+            // as long as the plumes do.
+            minLifetime = 1.0f;
+            maxLifetime = 2.5f;
+
+            minScale = 1.0f;
+            maxScale = 2.0f;
+
+            minNumParticles = 10;
+            maxNumParticles = 20;
+
+            minRotationSpeed = -MathHelper.PiOver4;
+            maxRotationSpeed = MathHelper.PiOver4;
+
+            spriteBlendMode = RenderSpriteBlendMode.AlphaBlendTop;
+
+            //DrawOrder = AlphaBlendDrawOrder;
+        }
+    }
+}
