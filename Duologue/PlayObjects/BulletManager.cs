e742491977a5b8e053839b46679567ee987c5a9e
Sam Hart <criswellious@gmail.com>
2009-01-06 09:03:42 -0500

    Add a bullet manager

---
+++ Duologue/PlayObjects/BulletManager.cs
@@ -0,0 +1,49 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+//using Mimicware;
+//using Mimicware.Graphics;
+//using Mimicware.Manager;
+// Duologue
+//using Duologue.State;
+#endregion
+
+namespace Duologue.PlayObjects
+{
+    public class BulletManager
+    {
+        #region Constants
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor / Init
+        public BulletManager()
+        {
+        }
+        #endregion
+
+        #region Private methods
+        #endregion
+
+        #region Public Methods
+        #endregion
+    }
+}
