4398e680d0d0966ed2c0bb4232f9f5b533f779b6
Sam Hart <criswellious@gmail.com>
2009-01-27 16:14:26 -0500

    More work on spitter

--- Duologue/PlayObjects/Enemy_Spitter.cs
+++ Duologue/PlayObjects/Enemy_Spitter.cs
@@ -26,21 +26,42 @@ namespace Duologue.PlayObjects
 {
     class Enemy_Spitter : Enemy
     {
+        public enum SpitterState
+        {
+            Spawning,
+            Alive,
+        }
         #region Constants
         private const string filename_base = "Enemies/spitter/0{0}-base";
         private const string filename_outline = "Enemies/spitter/0{0}-outline";
         private const string filename_spawnExplode = "Enemies/spitter/spawn-explode";
+        private const string filename_spit = "Enemies/spitter/spit-{0}";
 
         /// <summary>
         /// Max number of frames of animation
         /// </summary>
-        private const int maxFrames = 5;
+        private const int maxAnimationFrames = 5;
+
+        /// <summary>
+        /// Max number of frames for the spit
+        /// </summary>
+        private const int maxSpitFrames = 3;
         #endregion
 
         #region Fields
+        private Texture2D[] textureBase;
+        private Texture2D[] textureOutline;
+        private Texture2D[] textureSpit;
+        private Texture2D textureSpawnExplode;
+
+        private int currentFrame;
+        private byte[] currentSpitAlphas;
+
+        private float rotation;
         #endregion
 
         #region Properties
+        public SpitterState MyState;
         #endregion
 
         #region Constructor / Init
@@ -49,7 +70,7 @@ namespace Duologue.PlayObjects
         {
             MyType = TypesOfPlayObjects.Enemy_Spitter;
             MajorType = MajorPlayObjectType.Enemy;
-            RealSize = new Vector2(82, 90);
+            RealSize = new Vector2(85, 106);
             Initialized = false;
             Alive = false;
         }
@@ -61,11 +82,72 @@ namespace Duologue.PlayObjects
             ColorPolarity startColorPolarity,
             int? hitPoints)
         {
-            throw new NotImplementedException();
+            Position = startPos;
+            Orientation = startOrientation;
+            ColorState = currentColorState;
+            ColorPolarity = startColorPolarity;
+            if (hitPoints == null)
+            {
+                hitPoints = 0;
+            }
+            StartHitPoints = (int)hitPoints;
+            CurrentHitPoints = (int)hitPoints;
+
+            LoadAndInitialize();
         }
         #endregion
 
         #region Private methods
+        private void LoadAndInitialize()
+        {
+            textureBase = new Texture2D[maxAnimationFrames];
+            textureOutline = new Texture2D[maxAnimationFrames];
+            textureSpit = new Texture2D[maxSpitFrames];
+
+            for (int i = 0; i < maxAnimationFrames; i++)
+            {
+                textureBase[i] = InstanceManager.AssetManager.LoadTexture2D(String.Format(filename_base, (i + 1).ToString()));
+                textureOutline[i] = InstanceManager.AssetManager.LoadTexture2D(String.Format(filename_outline, (i + 1).ToString()));
+            }
+
+            for (int i = 0; i < maxSpitFrames; i++)
+            {
+                textureSpit[i] = InstanceManager.AssetManager.LoadTexture2D(String.Format(filename_spit, (i + 1).ToString()));
+            }
+
+            textureSpawnExplode = InstanceManager.AssetManager.LoadTexture2D(filename_spawnExplode);
+
+            Orientation = GetStartingVector();
+            rotation = MWMathHelper.ComputeAngleAgainstX(Orientation);
+
+            MyState = SpitterState.Spawning;
+
+            Initialized = true;
+        }
+
+        /// <summary>
+        /// Returns a vector pointing to the origin
+        /// </summary>
+        private Vector2 GetVectorPointingAtOrigin()
+        {
+            Vector2 sc = new Vector2(
+                    InstanceManager.DefaultViewport.Width / 2f,
+                    InstanceManager.DefaultViewport.Height / 2f);
+            return sc - Position;
+        }
+
+        /// <summary>
+        /// Get a starting vector for this dude
+        /// </summary>
+        private Vector2 GetStartingVector()
+        {
+            // Just aim at the center of screen for now
+            Vector2 temp = GetVectorPointingAtOrigin() + new Vector2(
+                (float)MWMathHelper.GetRandomInRange(-.5, .5),
+                (float)MWMathHelper.GetRandomInRange(-.5, .5));
+            temp.Normalize();
+            return temp;
+        }
         #endregion
 
         #region Public Methods
