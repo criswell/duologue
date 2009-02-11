57751f5f55bf8f69495b011f40e930fd2edfb9b3
Sam Hart <criswellious@gmail.com>
2009-02-11 17:03:05 -0500

    Add stubbed gloop king

---
+++ Duologue/PlayObjects/Enemy_GloopKing.cs
@@ -0,0 +1,84 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Graphics;
+using Mimicware.Manager;
+// Duologue
+using Duologue.State;
+using Duologue.Screens;
+#endregion
+
+namespace Duologue.PlayObjects
+{
+    class Enemy_GloopKing : Enemy
+    {
+        #region Constants
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Construct/ Init
+        public override void Initialize(
+            Vector2 startPos, 
+            Vector2 startOrientation, 
+            ColorState currentColorState, 
+            ColorPolarity startColorPolarity, 
+            int? hitPoints)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Public Overrides
+        public override bool StartOffset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UpdateOffset(PlayObject pobj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ApplyOffset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool TriggerHit(PlayObject pobj)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Draw / Update
+        public override void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
