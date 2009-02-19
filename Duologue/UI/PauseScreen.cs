47484f4e42695b4b079a6565a852fe5f30316a0f
Sam Hart <criswellious@gmail.com>
2009-02-19 01:32:54 -0500

    Work on the pause screen

--- Duologue/UI/PauseScreen.cs
+++ Duologue/UI/PauseScreen.cs
@@ -1,16 +1,31 @@
+#region File Description
+#endregion
+
+#region Using Statements
+// System
 using System;
 using System.Collections.Generic;
-using System.Linq;
+// XNA
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
-
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Manager;
+using Mimicware.Graphics;
+// Duologue
+using Duologue;
+using Duologue.Properties;
+using Duologue.Screens;
+using Duologue.PlayObjects;
+using Duologue.Waves;
+using Duologue.UI;
+using Duologue.State;
+#endregion
 
 namespace Duologue.UI
 {
@@ -20,9 +35,36 @@ namespace Duologue.UI
     public class PauseScreen : Microsoft.Xna.Framework.DrawableGameComponent
     {
         #region Constants
+        private const string filename_overlay = "pause-overlay";
+        private const string filename_font = "Fonts/inero-28";
+
+        private const byte overlayAlpha = 128;
+
+        private const byte maxTextRed = 255;
+        private const byte minTextRed = 50;
+        private const int maxDeltaTextRed = 20;
+
+        private const int numberOfUpdatesPerTick = 5;
         #endregion
 
         #region Fields
+        private Texture2D overlay;
+        private SpriteFont font;
+
+        private Vector2 fontPosition;
+        private int numberOfTiles;
+        private Vector2 tileCounts;
+
+        private int screenWidth;
+        private int screenHeight;
+
+        private SpriteEffects[] tileEffects;
+
+        private Color color_overlay;
+        private Color color_text;
+        private Color color_outline;
+        private Vector2[] shadowOffset;
+        private int deltaTextRed;
         #endregion
 
         #region Properties
@@ -41,14 +83,62 @@ namespace Duologue.UI
         /// </summary>
         public override void Initialize()
         {
-            // TODO: Add your initialization code here
+            shadowOffset = new Vector2[2];
+            shadowOffset[0] = Vector2.One;
+            shadowOffset[1] = -1 * Vector2.One;
+
+            color_outline = Color.Gray;
+
+            color_text = new Color(maxTextRed, 0, 0);
+            deltaTextRed = -1 * maxDeltaTextRed;
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            overlay = InstanceManager.AssetManager.LoadTexture2D(filename_overlay);
+            font = InstanceManager.AssetManager.LoadSpriteFont(filename_font);
+
+            numberOfTiles = -1;
+
+            color_overlay = new Color(Color.White, overlayAlpha);
+
             base.LoadContent();
+        }
+        #endregion
+
+        #region Private Methods
+        private void InitAll()
+        {
+            screenWidth = InstanceManager.DefaultViewport.Width;
+            screenHeight = InstanceManager.DefaultViewport.Height;
+
+            // Setup the tile stuff
+            tileCounts = new Vector2(
+                (float)Math.Ceiling((double)screenWidth / (double)overlay.Width),
+                (float)Math.Ceiling((double)screenHeight / (double)overlay.Height));
+
+            numberOfTiles = (int)(tileCounts.X * tileCounts.Y);
+
+            tileEffects = new SpriteEffects[numberOfTiles];
+            for (int i = 0; i < numberOfTiles; i++)
+            {
+                JumbleTile(i);
+            }
+
+            // Setup the font stuff
+            fontPosition = new Vector2(
+                screenWidth / 2f - font.MeasureString(Resources.PauseScreen_GamePaused).X / 2f,
+                screenHeight / 2f - font.MeasureString(Resources.PauseScreen_GamePaused).Y / 2f);
+        }
+
+        private void JumbleTile(int i)
+        {
+            if (MWMathHelper.CoinToss())
+                tileEffects[i] = SpriteEffects.FlipHorizontally;
+            else
+                tileEffects[i] = SpriteEffects.None;
         }
         #endregion
 
@@ -59,13 +149,89 @@ namespace Duologue.UI
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            // TODO: Add your update code here
+            // We only want to proceed provided the InitAll() was called
+            // Since we have no guarantee that the screen is initialized here in
+            // update, we ultimately will have to pass until Draw() is called
+            if (numberOfTiles > 0)
+            {
+                // Update the font color
+                int t = (int)color_text.R + deltaTextRed;
+
+                if (t > maxTextRed)
+                {
+                    t = (int)maxTextRed;
+                    deltaTextRed = -1 * maxDeltaTextRed;
+                }
+                else if (t < minTextRed)
+                {
+                    t = (int)minTextRed;
+                    deltaTextRed = maxDeltaTextRed;
+                }
+
+                color_text = new Color((byte)t, 0, 0);
+
+                // Update the overlays
+                int i;
+                for (t = 0; t < numberOfUpdatesPerTick; t++)
+                {
+                    i = MWMathHelper.GetRandomInRange(0, numberOfTiles);
+                    if (tileEffects[i] == SpriteEffects.None)
+                        tileEffects[i] = SpriteEffects.FlipHorizontally;
+                    else
+                        tileEffects[i] = SpriteEffects.None;
+                }
+            }
 
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
+            if (numberOfTiles <= 0)
+            {
+                InitAll();
+            }
+
+            float x = 0f;
+            float y = 0f;
+            // Draw the tiles
+            for (int i = 0; i < numberOfTiles; i++)
+            {
+                InstanceManager.RenderSprite.Draw(
+                    overlay,
+                    new Vector2(x, y),
+                    Vector2.Zero,
+                    null,
+                    color_overlay,
+                    0f,
+                    1f,
+                    0f,
+                    RenderSpriteBlendMode.AlphaBlendTop,
+                    tileEffects[i]);
+
+                x += overlay.Width;
+                if (x >= screenWidth)
+                {
+                    x = 0f;
+                    y += overlay.Height;
+                    if (y >= screenHeight)
+                    {
+                        // okay, wtf, we shouldn't be here
+                        y = 0f;
+                    }
+                }
+            }
+
+            // Draw the text
+            InstanceManager.RenderSprite.DrawString(
+                font,
+                Resources.PauseScreen_GamePaused,
+                fontPosition,
+                color_text,
+                color_outline,
+                shadowOffset,
+                RenderSpriteBlendMode.AlphaBlendTop);
+
             base.Draw(gameTime);
         }
         #endregion
