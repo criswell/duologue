1ed60655963a0aac0fe375bfebb8c8465f484537
Sam Hart <criswellious@gmail.com>
2009-02-20 10:22:39 -0500

    Add stubbed out window manager class

---
+++ Duologue/UI/WindowManager.cs
@@ -0,0 +1,63 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Manager;
+using Mimicware.Graphics;
+// Duologue
+using Duologue;
+using Duologue.Properties;
+using Duologue.Screens;
+using Duologue.PlayObjects;
+using Duologue.Waves;
+using Duologue.UI;
+using Duologue.State;
+#endregion
+
+namespace Duologue.UI
+{
+    class WindowManager
+    {
+        #region Constants
+        private const string filename_bottom = "PlayerUI/ui-widget-bottom";
+        private const string filename_left = "PlayerUI/ui-widget-left";
+        private const string filename_right = "PlayerUI/ui-widget-right";
+        private const string filename_top = "PlayerUI/ui-widget-top";
+
+        private const string filename_bottomleft = "PlayerUI/ui-widget-bottomleft";
+        private const string filename_bottomright = "PlayerUI/ui-widget-bottomright";
+        private const string filename_topleft = "PlayerUI/ui-widget-topleft";
+        private const string filename_topright = "PlayerUI/ui-widget-topright";
+
+        private const string filename_text = "PlayerUI/ui-widget-text-{0}";
+        #endregion
+
+        #region Feilds
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor / Init
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+        #region Public Methods
+        #endregion
+    }
+}
