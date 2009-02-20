916039bf0786de7b1e3b6c4635048fc59a61f134
Sam Hart <criswellious@gmail.com>
2009-02-20 11:29:05 -0500

    Work on init in WM

--- Duologue/UI/WindowManager.cs
+++ Duologue/UI/WindowManager.cs
@@ -43,15 +43,78 @@ namespace Duologue.UI
         private const string filename_topright = "PlayerUI/ui-widget-topright";
 
         private const string filename_text = "PlayerUI/ui-widget-text-{0}";
+
+        private const string filename_screen = "PlayerUI/ui-screen";
+
+        private const int numberOfTextFrames = 2;
         #endregion
 
-        #region Feilds
+        #region Fields
+        private Texture2D frameBottom;
+        private Texture2D frameLeft;
+        private Texture2D frameRight;
+        private Texture2D frameTop;
+
+        private Texture2D cornerBottomLeft;
+        private Texture2D cornerBottomRight;
+        private Texture2D cornerTopLeft;
+        private Texture2D cornerTopRight;
+
+        private Texture2D screenOverlay;
+
+        private Texture2D[] textFrames;
+
+        private Vector2 position;
+        private Vector2 windowSize;
+
+        private Rectangle screenRect;
+        private Vector2 screenOffset;
+        private Vector2 screenSizeOffset;
+
+        private Vector2 minimumSize;
         #endregion
 
         #region Properties
         #endregion
 
         #region Constructor / Init
+        public WindowManager()
+        {
+            LoadContents();
+
+            position = Vector2.Zero;
+            windowSize = Vector2.Zero;
+            screenRect = Rectangle.Empty;
+        }
+
+        private void LoadContents()
+        {
+            frameBottom = InstanceManager.AssetManager.LoadTexture2D(filename_bottom);
+            frameLeft = InstanceManager.AssetManager.LoadTexture2D(filename_left);
+            frameRight = InstanceManager.AssetManager.LoadTexture2D(filename_right);
+            frameTop = InstanceManager.AssetManager.LoadTexture2D(filename_top);
+
+            cornerBottomLeft = InstanceManager.AssetManager.LoadTexture2D(filename_bottomleft);
+            cornerBottomRight = InstanceManager.AssetManager.LoadTexture2D(filename_bottomright);
+            cornerTopLeft = InstanceManager.AssetManager.LoadTexture2D(filename_topleft);
+            cornerTopRight = InstanceManager.AssetManager.LoadTexture2D(filename_topright);
+
+            screenOverlay = InstanceManager.AssetManager.LoadTexture2D(filename_screen);
+
+            textFrames = new Texture2D[numberOfTextFrames];
+            for (int i = 0; i < numberOfTextFrames; i++)
+            {
+                textFrames[i] = InstanceManager.AssetManager.LoadTexture2D(String.Format(filename_text, i.ToString()));
+            }
+
+            screenOffset = new Vector2(
+                cornerTopLeft.Width / 2f,
+                cornerTopLeft.Height / 2f);
+
+            screenSizeOffset = new Vector2(
+                cornerBottomLeft.Width / 2f,
+                cornerBottomLeft.Height / 2f);
+        }
         #endregion
 
         #region Private Methods
