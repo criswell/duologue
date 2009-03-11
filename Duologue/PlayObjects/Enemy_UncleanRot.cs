725cee900644e79f9f0fc036b21afbdc35b16470
Sam Hart <criswellious@gmail.com>
2009-03-11 02:40:51 -0400

    Flesh out the init section of unclean rot. add unclean rot to play object list

--- Duologue/PlayObjects/Enemy_UncleanRot.cs
+++ Duologue/PlayObjects/Enemy_UncleanRot.cs
@@ -27,15 +27,51 @@ namespace Duologue.PlayObjects
     public class Enemy_UncleanRot : Enemy
     {
         #region Constants
+        // Filenames
+        private const string filename_Base = "Enemies/unclean_rot/base-{0}";
+        private const string filename_Outline = "Enemies/unclean_rot/outline_{0}";
+        private const string filename_OutlineTongue = "Enemies/unclean_rot/outline-tongue-{0}";
+        private const string filename_Skullcap = "Enemies/unclean_rot/skullcap-{0}";
+        private const string filename_Static = "Enemies/unclean_rot/static-{0:00}";
+
+        private const int numFrames_Body = 9;
+        private const int numFrames_Tongue = 6;
+        private const int numFrames_Static = 10;
+
+        /// <summary>
+        /// This is both the minimum number of hit points it is possible for this boss to have
+        /// as well as the step-size for each additional hitpoint requested.
+        /// E.g., if you request this boss have "2" HP, then he will *really* get "2 x realHitPointMultiplier" HP
+        /// </summary>
+        private const int realHitPointMultiplier = 5;
         #endregion
 
         #region Fields
+        private Texture2D[] texture_Base;
+        private Texture2D[] texture_Outline;
+        private Texture2D[] texture_OutlineTongue;
+        private Texture2D[] texture_Skullcap;
+        private Texture2D[] texture_Static;
+
+        private Vector2 center_Body;
+        private Vector2 center_Static;
         #endregion
 
         #region Properties
         #endregion
 
         #region Constructor / Init
+        public Enemy_UncleanRot(GamePlayScreenManager manager)
+            : base(manager)
+        {
+            MyType = TypesOfPlayObjects.Enemy_UncleanRot;
+            MajorType = MajorPlayObjectType.Enemy;
+            MyEnemyType = EnemyType.Leader;
+            RealSize = new Vector2(300, 280);
+            Initialized = false;
+            Alive = false;
+        }
+
         public override void Initialize(
             Vector2 startPos, 
             Vector2 startOrientation, 
@@ -43,7 +79,55 @@ namespace Duologue.PlayObjects
             ColorPolarity startColorPolarity, 
             int? hitPoints)
         {
-            throw new NotImplementedException();
+            Position = startPos;
+            Orientation = startOrientation;
+            ColorState = currentColorState;
+            ColorPolarity = startColorPolarity;
+            if (hitPoints == null)
+            {
+                hitPoints = 1;
+            }
+            StartHitPoints = (int)hitPoints * realHitPointMultiplier;
+            CurrentHitPoints = (int)hitPoints * realHitPointMultiplier;
+            //audio = ServiceLocator.GetService<AudioManager>();
+            LoadAndInitialize();
+        }
+
+        private void LoadAndInitialize()
+        {
+            texture_Base = new Texture2D[numFrames_Body];
+            texture_Outline = new Texture2D[numFrames_Body];
+            texture_Skullcap = new Texture2D[numFrames_Body];
+            texture_OutlineTongue = new Texture2D[numFrames_Tongue];
+            texture_Static = new Texture2D[numFrames_Static];
+
+            for (int i = 0; i < numFrames_Body; i++)
+            {
+                texture_Base[i] = InstanceManager.AssetManager.LoadTexture2D(
+                    String.Format(filename_Base, (i + 1).ToString()));
+                texture_Outline[i] = InstanceManager.AssetManager.LoadTexture2D(
+                    String.Format(filename_Outline, (i + 1).ToString()));
+                texture_Skullcap[i] = InstanceManager.AssetManager.LoadTexture2D(
+                    String.Format(filename_Skullcap, (i + 1).ToString()));
+            }
+
+            for (int i = 0; i < numFrames_Tongue; i++)
+            {
+                texture_OutlineTongue[i] = InstanceManager.AssetManager.LoadTexture2D(
+                    String.Format(filename_OutlineTongue, (i + 1).ToString()));
+            }
+
+            center_Body = new Vector2(
+                texture_Base[0].Width / 2f, texture_Base[0].Height / 2f);
+
+            for (int i = 0; i < numFrames_Static; i++)
+            {
+                texture_Static[i] = InstanceManager.AssetManager.LoadTexture2D(
+                    String.Format(filename_Static, (i + 1).ToString()));
+            }
+
+            center_Static = new Vector2(
+                texture_Static[0].Width / 2f, texture_Static[0].Height / 2f);
         }
         #endregion
 
