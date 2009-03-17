5e92fac308bc3fbcab9f24141e96b15548cf33f8
Sam Hart <criswellious@gmail.com>
2009-03-17 12:38:16 -0400

    Begin to make an intro screen. Add copyright text to resources

---
+++ Duologue/Screens/CompanyIntroScreenManager.cs
@@ -0,0 +1,60 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware.Manager;
+using Mimicware.Graphics;
+using Mimicware;
+// Duologue
+using Duologue;
+using Duologue.Audio;
+using Duologue.State;
+using Duologue.Properties;
+using Duologue.Screens;
+using Duologue.PlayObjects;
+using Duologue.Waves;
+using Duologue.UI;
+#endregion
+
+namespace Duologue.Screens
+{
+    public class CompanyIntroScreenManager : GameScreen
+    {
+        #region Constants
+        #endregion
+
+        #region Fields
+        #endregion 
+
+        #region Properties
+        #endregion
+
+        #region Constructor / Init
+        protected override void InitializeConstants()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Public Overrides
+        #endregion
+
+        #region Update
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        #endregion
+    }
+}
