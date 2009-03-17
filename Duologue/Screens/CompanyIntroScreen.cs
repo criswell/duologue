51d727af10095f0ede46fdf08afe9eaeb7578020
Sam Hart <criswellious@gmail.com>
2009-03-17 13:28:53 -0400

    Add a drawable component for the company intro

---
+++ Duologue/Screens/CompanyIntroScreen.cs
@@ -0,0 +1,29 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware.Manager;
+using Mimicware.Graphics;
+// Duologue
+using Duologue;
+using Duologue.Properties;
+#endregion
+
+namespace Duologue.Screens
+{
+    public class CompanyIntroScreen : DrawableGameComponent
+    {
+    }
+}
