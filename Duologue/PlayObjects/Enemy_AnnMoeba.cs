c352fd76e1dde7c23f904ce1ddf3531206e5c3f3
Sam Hart <criswellious@gmail.com>
2009-03-26 12:48:00 -0400

    Add stubbed out AnnMoeba enemy

---
+++ Duologue/PlayObjects/Enemy_AnnMoeba.cs
@@ -0,0 +1,74 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Graphics;
+using Mimicware.Manager;
+// Duologue
+using Duologue.State;
+using Duologue.Screens;
+#endregion
+
+namespace Duologue.PlayObjects
+{
+    public class Enemy_AnnMoeba :Enemy
+    {
+        public override void Initialize(
+            Vector2 startPos, 
+            Vector2 startOrientation, 
+            ColorState currentColorState, 
+            ColorPolarity startColorPolarity, 
+            int? hitPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool StartOffset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UpdateOffset(PlayObject pobj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ApplyOffset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetFilenames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool TriggerHit(PlayObject pobj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
