db70b135073317944e4711ab4b89be6dd5e998bb
Sam Hart <criswellious@gmail.com>
2009-03-26 12:41:24 -0400

    bah Brian Farts

--- Duologue/Background.cs
+++ Duologue/Background.cs
@@ -389,7 +389,7 @@ namespace Duologue
         {
             lastBackground = currentBackground;
             if (backgroundNum >= numBackgrounds)
-                backgroundNum = numBackgrounds % backgroundNum - 1;
+                backgroundNum = (numBackgrounds-1) % backgroundNum;
 
             currentBackground = backgroundNum;
             timeSinceTransitionRequest = 0f;
