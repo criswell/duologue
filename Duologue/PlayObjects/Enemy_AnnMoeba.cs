74dfd3e02fcae251f6b46933127fdd0bfb35192a
Sam Hart <criswellious@gmail.com>
2009-03-26 17:25:55 -0400

    Add bubble to project. work more on the annmoeba

--- Duologue/PlayObjects/Enemy_AnnMoeba.cs
+++ Duologue/PlayObjects/Enemy_AnnMoeba.cs
@@ -30,6 +30,9 @@ namespace Duologue.PlayObjects
         private const string filename_Glooplet = "Enemies/gloop/glooplet";
         private const string filename_GloopletHighlight = "Enemies/gloop/glooplet-highlight";
         private const string filename_Death = "Enemies/gloop/glooplet-death";
+        private const string filename_Bubble = "Enemies/iridescent_bubble";
+
+        private const float bubbleScale = 0.43f;
 
         private const float radiusMultiplier = 0.8f;
 
@@ -41,8 +44,11 @@ namespace Duologue.PlayObjects
         private const double minGlobuleScale = 0.1;
         private const double maxGlobuleScale = 0.4;
 
-        private const double minGlobuleSpeed = -2.0;
-        private const double maxGlobuleSpeed = 2.0;
+        private const double minGlobuleAddition = -1.0;
+        private const double maxGlobuleAddition = 1.0;
+
+        private const double minGlobuleMultiplication = -3.0;
+        private const double maxGlobuleMultiplication = 3.0;
         #region Force interactions
         #endregion
         #endregion
@@ -54,13 +60,20 @@ namespace Duologue.PlayObjects
         private Texture2D texture_Glooplet;
         private Texture2D texture_Highlight;
         private Texture2D texture_Death;
+        private Texture2D texture_Bubble;
+        private Vector2 center_Bubble;
         private Vector2 center_Glooplet;
         private Vector2 center_Highlight;
 
+        private Color color_Bubble;
+
         private Vector2[] offset_Globules;
-        private Vector2[] speed_Globules;
+        private Vector2[] phiOperands_Addition;
+        private Vector2[] phiOperands_Multiplication;
         private float[] scale_Globules;
         private float mainScale;
+
+        private double currentPhi;
         #endregion
 
         #region Constructor / Init
@@ -74,7 +87,7 @@ namespace Duologue.PlayObjects
         {
             MyType = TypesOfPlayObjects.Enemy_AnnMoeba;
             MajorType = MajorPlayObjectType.Enemy;
-            RealSize = new Vector2(83, 83);
+            RealSize = new Vector2(90, 87);
             Initialized = false;
             Alive = false;
         }
@@ -105,6 +118,9 @@ namespace Duologue.PlayObjects
             texture_Glooplet = InstanceManager.AssetManager.LoadTexture2D(filename_Glooplet);
             texture_Death = InstanceManager.AssetManager.LoadTexture2D(filename_Death);
             texture_Highlight = InstanceManager.AssetManager.LoadTexture2D(filename_GloopletHighlight);
+            texture_Bubble = InstanceManager.AssetManager.LoadTexture2D(filename_Bubble);
+            center_Bubble = new Vector2(
+                texture_Bubble.Width / 2f, texture_Bubble.Height / 2f);
             center_Glooplet = new Vector2(
                 texture_Glooplet.Width / 2f, texture_Glooplet.Height / 2f);
             center_Highlight = new Vector2(
@@ -113,19 +129,25 @@ namespace Duologue.PlayObjects
             mainScale = (float)MWMathHelper.GetRandomInRange(minScale, maxScale);
             scale_Globules = new float[numberOfGlobules];
             offset_Globules = new Vector2[numberOfGlobules];
-            speed_Globules = new Vector2[numberOfGlobules];
+            phiOperands_Addition = new Vector2[numberOfGlobules];
+            phiOperands_Multiplication = new Vector2[numberOfGlobules];
 
             Radius = RealSize.Length() * mainScale * radiusMultiplier;
 
             for (int i = 0; i < numberOfGlobules; i++)
             {
                 scale_Globules[i] = (float)MWMathHelper.GetRandomInRange(minGlobuleScale, maxGlobuleScale);
-                offset_Globules[i] = new Vector2(
-                    (float)MWMathHelper.GetRandomInRange((double)(-Radius), (double)Radius),
-                    (float)MWMathHelper.GetRandomInRange((double)(-Radius), (double)Radius));
-                speed_Globules[i] = MWMathHelper.RotateVectorByRadians(Vector2.One, (float)MWMathHelper.GetRandomInRange(0, MathHelper.TwoPi));
-                speed_Globules[i] *= (float)MWMathHelper.GetRandomInRange(minGlobuleSpeed, maxGlobuleSpeed);
+                phiOperands_Addition[i] = new Vector2(
+                    (float)MWMathHelper.GetRandomInRange(minGlobuleAddition, maxGlobuleAddition),
+                    (float)MWMathHelper.GetRandomInRange(minGlobuleAddition, maxGlobuleAddition));
+                phiOperands_Multiplication[i] = new Vector2(
+                    (float)MWMathHelper.GetRandomInRange(minGlobuleMultiplication, maxGlobuleMultiplication),
+                    (float)MWMathHelper.GetRandomInRange(minGlobuleMultiplication, maxGlobuleMultiplication));
             }
+            currentPhi = 0;
+            ComputeGlobuleOffsets();
+
+            color_Bubble = new Color(2, 109, 74, 200);
 
             Initialized = true;
             Alive = true;
@@ -142,6 +164,14 @@ namespace Duologue.PlayObjects
         }
         #endregion
 
+        #region Private methods
+        private void ComputeGlobuleOffsets()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Public overrides
         public override bool StartOffset()
         {
             throw new NotImplementedException();
@@ -161,7 +191,9 @@ namespace Duologue.PlayObjects
         {
             throw new NotImplementedException();
         }
+        #endregion
 
+        #region Public Draw/Update
         public override void Draw(GameTime gameTime)
         {
             throw new NotImplementedException();
@@ -171,5 +203,6 @@ namespace Duologue.PlayObjects
         {
             throw new NotImplementedException();
         }
+        #endregion
     }
 }
