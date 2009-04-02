7cbdf00a24144c0cfd353265d8e39435d2af612c
Sam Hart <criswellious@gmail.com>
2009-04-02 14:05:16 -0400

    more work on the spawner

--- Duologue/PlayObjects/Enemy_Spawner.cs
+++ Duologue/PlayObjects/Enemy_Spawner.cs
@@ -31,7 +31,7 @@ namespace Duologue.PlayObjects
         Fleeing,
         FlareUp,
         FlareDown,
-        Scanning,
+        Scanning
     }
     public class Enemy_Spawner : Enemy
     {
@@ -52,9 +52,18 @@ namespace Duologue.PlayObjects
         private const float maxSpeed = 4f;
         private const float accel = 0.25f;
 
+        private const float fleeSpeed = 9.3f;
+
         private const double maxFleeDistanceMultiplier = 7.0;
         private const double minFleeDistanceMultiplier = 3.0;
         private const float radiusMultiplier = 0.78f;
+
+        private const float minFlareSize = 0.1f;
+        private const float maxFlareSzie = 0.89f;
+        private const float delta_FlareSize = 0.09f;
+
+        private const double timeBetweenColorSwitches = 0.33;
+        private const double timeToThink = 0.202;
         #endregion
 
         #region Fields
@@ -71,6 +80,20 @@ namespace Duologue.PlayObjects
         private SpawnerState currentState;
 
         private Vector2 nextPosition;
+        private float travelLength;
+        private float totalTravelLength;
+        private float speed;
+        private float size_Flare;
+
+        private Color color_Current;
+        private Color color_Base;
+        private int[] colorIndex_Lights;
+        private Color[] color_PossibleLightColors;
+
+        private double timer_ColorSwitchCountdown;
+        private double timer_Thinking;
+        private TypesOfPlayObjects[] possibleNigglets;
+        private List<TypesOfPlayObjects> weightedNiggletList;
         #endregion
 
         #region Properties
@@ -136,8 +159,47 @@ namespace Duologue.PlayObjects
             Radius = RealSize.Length() * radiusMultiplier;
 
             currentState = SpawnerState.Moving;
+            speed = 0f;
+            size_Flare = minFlareSize;
+
+            color_Current = GetMyColor(ColorState.Light);
+            color_Base = Color.Silver;
+            colorIndex_Lights = new int[numOfLightLevels];
+            color_PossibleLightColors = new Color[]{
+                Color.MediumAquamarine,
+                Color.MediumVioletRed,
+                Color.OrangeRed,
+                Color.GreenYellow,
+                Color.DeepPink,
+                Color.Yellow,
+                Color.Violet,
+                Color.Turquoise,
+                Color.SteelBlue,
+            };
+
+            SetLightColors();
+            timer_ColorSwitchCountdown = 0;
 
             GetNextPosition(Vector2.Zero);
+
+            // To give more weight to specific types, list
+            // them multiple times
+            possibleNigglets = new TypesOfPlayObjects[]
+            {
+                TypesOfPlayObjects.Enemy_Buzzsaw,
+                TypesOfPlayObjects.Enemy_Ember,
+                TypesOfPlayObjects.Enemy_Gloop,
+                TypesOfPlayObjects.Enemy_Maggot,
+                TypesOfPlayObjects.Enemy_Mirthworm,
+                TypesOfPlayObjects.Enemy_Mirthworm,
+                TypesOfPlayObjects.Enemy_Mirthworm,
+                TypesOfPlayObjects.Enemy_Maggot,
+                TypesOfPlayObjects.Enemy_Roggles,
+                TypesOfPlayObjects.Enemy_Spitter,
+                TypesOfPlayObjects.Enemy_Wiggles,
+            };
+
+            weightedNiggletList = new List<TypesOfPlayObjects>();
 
             Alive = true;
             Initialized = true;
@@ -180,26 +242,141 @@ namespace Duologue.PlayObjects
 
                 nextPosition = Position + distance * vector2;
             }
+
+            Orientation = nextPosition - Position;
+            totalTravelLength = Orientation.Length();
+            Orientation.Normalize();
+            travelLength = 0;
+        }
+
+        private void SetLightColors()
+        {
+            for (int i = 0; i < numOfLightLevels; i++)
+            {
+                colorIndex_Lights[i] = MWMathHelper.GetRandomInRange(0, color_PossibleLightColors.Length);
+            }
+        }
+
+        /*
+            __________________________________________________________________
+            __________________________________________________________________
+            ______________________________,..ucz..,___________________________
+            ________________________ _,@{5E3[?}P~s[3X2,_______________________
+            _____________________  ,{51R]F!?!h;:1c:7!c!P1.   _________________
+            ___________________  ,{Et7!5lJ:.: !ciI.c;5}L:31.  ________________
+            ___________________ :F:KrL.::.!=!:=zr;:3;CiH.::5i_________________
+            _________________  ;1:]h;zzzzC!c;utL.nz5iuj?j:::!U  ______________
+            _________________ .H.JlJ[VTC{ClD{2CU7C}P+P.;{L:::3. ______________
+            _________________ :L:E)=l;}L!5[5}Z}F(C)/]:.!.Q.::J[.______________
+            _________________ !L!E \.7;;}FF3TE[k;7!~!;.:;U.:::)L______________
+            _________________  hiE    :U.F!:::!?(7:c(3ihtQ]c;J).______________
+            _________________ :Utn:. \:L:ztzzn..:L;ni::;|hzCjJ( ______________
+            _________________ :H\:). ==UtOtttntht.:Y.;=Utb!V;H) ______________
+            _________________ 'KjK]J[h.7" :==c:.::tyzU{Ht:}Q$F` ______________
+            _________________      G[sjJ.={3[F{L:[{2{U1H.@7   ________________
+            ___________________ _,.3}H73FE}EFF+F1:::jH}B]F  __________________
+            _________________  /1Hlp!F!h;;:::::::::J1E1:]R_ __________________
+            _________________ :5!Ut5t:;.:.:ti..z;.!EiQ(:(F:c. ________________
+            ________________.x\=:^1X!::z(hthi==CjDjh1UiJ(:i !nz.______________
+            _________ __.ntP!.  -:!k.U[C!:!.:;:=tJ[H[UyG(O!\.C) !ft.,_ _______
+            _______ _y1P7 :       .=TQ{P.j:.:.{3!L}g{R}2[FT3[L]J[Z77!Rj_______
+            _______,1F):  !;.=7    :.g{E]H]U}n[3}HEF}PyE[../1FTFl-:   !L".__ _
+            ___  gcEcu+<cpyL.pi::==FF::!}3l3iE!ItEiH1b1niE5ciFyuyscuyE=E(=(5_ 
+            ___,;P|H("|JtL(Lib:b!:jJtYjb1HtcfU1Ht5tFtc]u.QiE)L(5ik=U)u1Utht.!L
+            _  z':fOtU1JtDtUeU1Ojd1JthtO1dt:.CtX.x;Jtht43hcJzV)Q1UtL.D=U V(.tU
+
+                                T H A T ' S   R A C I S T !
+        */
+        private void SpawnNigglet(GameTime gameTime)
+        {
+            weightedNiggletList.Clear();
         }
         #endregion
 
         #region Public overrides
         public override bool StartOffset()
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public override bool UpdateOffset(PlayObject pobj)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public override bool ApplyOffset()
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public override bool TriggerHit(PlayObject pobj)
+        {
+            return true;
+        }
+        #endregion
+
+        #region Private Update Methods
+        private void Update_Rotate()
+        {
+            if (currentState == SpawnerState.Fleeing)
+            {
+                rotation_InnerRing += delta_InnerRingFlee;
+                rotation_OuterRing += delta_OuterRingFlee;
+            }
+            else
+            {
+                rotation_InnerRing += delta_InnerRingNormal;
+                rotation_OuterRing += delta_OuterRingNormal;
+            }
+
+            if (rotation_InnerRing > MathHelper.TwoPi)
+                rotation_InnerRing = 0;
+            else if (rotation_InnerRing < 0)
+                rotation_InnerRing = MathHelper.TwoPi;
+
+            if (rotation_OuterRing > MathHelper.TwoPi)
+                rotation_OuterRing = 0;
+            else if (rotation_OuterRing < 0)
+                rotation_OuterRing = MathHelper.TwoPi;
+        }
+
+        private void Update_Moving(GameTime gameTime)
+        {
+            if (speed < maxSpeed)
+                speed += accel;
+
+            Position += speed * Orientation;
+            travelLength += speed;
+            if (travelLength > totalTravelLength)
+            {
+                travelLength = 0;
+                speed = 0;
+                timer_Thinking = 0;
+                currentState = SpawnerState.Scanning;
+            }
+        }
+
+        private void Update_Scanning(GameTime gameTime)
+        {
+            timer_Thinking += gameTime.ElapsedGameTime.TotalSeconds;
+            if (timer_Thinking > timeToThink)
+            {
+                timer_Thinking = 0;
+                SpawnNigglet(gameTime);
+            }
+        }
+
+        private void Update_Fleeing(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update_FlareDown(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update_FlareUp(GameTime gameTime)
         {
             throw new NotImplementedException();
         }
@@ -208,12 +385,101 @@ namespace Duologue.PlayObjects
         #region Draw / Update
         public override void Draw(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            if (currentState == SpawnerState.FlareUp || currentState == SpawnerState.FlareDown)
+            {
+                InstanceManager.RenderSprite.Draw(
+                    texture_Flare,
+                    Position,
+                    center_Flare,
+                    null,
+                    color_Current,
+                    0f,
+                    size_Flare,
+                    0f,
+                    RenderSpriteBlendMode.AlphaBlend);
+            }
+
+            // Draw base of the ship
+            InstanceManager.RenderSprite.Draw(
+                texture_Base,
+                Position,
+                center_Base,
+                null,
+                color_Base,
+                rotation_OuterRing,
+                1f,
+                0f,
+                RenderSpriteBlendMode.AlphaBlendTop);
+
+            InstanceManager.RenderSprite.Draw(
+                texture_Base,
+                Position,
+                center_Base,
+                null,
+                color_Base,
+                rotation_InnerRing,
+                scale_InnerRing,
+                0f,
+                RenderSpriteBlendMode.AlphaBlendTop);
+
+            // Draw the lights;
+            for (int i = 0; i < numOfLightLevels; i++)
+            {
+                InstanceManager.RenderSprite.Draw(
+                    texture_Lights[i],
+                    Position,
+                    center_Base,
+                    null,
+                    color_PossibleLightColors[colorIndex_Lights[i]],
+                    rotation_OuterRing,
+                    1f,
+                    0f,
+                    RenderSpriteBlendMode.AlphaBlendTop);
+
+                InstanceManager.RenderSprite.Draw(
+                    texture_Lights[i],
+                    Position,
+                    center_Base,
+                    null,
+                    color_PossibleLightColors[colorIndex_Lights[i]],
+                    rotation_InnerRing,
+                    scale_InnerRing,
+                    0f,
+                    RenderSpriteBlendMode.AlphaBlendTop);
+            }
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            timer_ColorSwitchCountdown += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (timer_ColorSwitchCountdown > timeBetweenColorSwitches)
+            {
+                timer_ColorSwitchCountdown = 0;
+                SetLightColors();
+            }
+
+            Update_Rotate();
+
+            switch (currentState)
+            {
+                case SpawnerState.FlareUp:
+                    Update_FlareUp(gameTime);
+                    break;
+                case SpawnerState.FlareDown:
+                    Update_FlareDown(gameTime);
+                    break;
+                case SpawnerState.Fleeing:
+                    Update_Fleeing(gameTime);
+                    break;
+                case SpawnerState.Scanning:
+                    Update_Scanning(gameTime);
+                    break;
+                default:
+                    // Plain old moving
+                    Update_Moving(gameTime);
+                    break;
+            }
         }
         #endregion
     }
