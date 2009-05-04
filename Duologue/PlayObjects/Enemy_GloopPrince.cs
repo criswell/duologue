219480549d0d8f70c74cc6b6a6dd19ee72253ae3
Sam Hart <criswellious@gmail.com>
2009-05-04 13:53:35 -0400

    work on the prince gloop

--- Duologue/PlayObjects/Enemy_GloopPrince.cs
+++ Duologue/PlayObjects/Enemy_GloopPrince.cs
@@ -34,12 +34,46 @@ namespace Duologue.PlayObjects
         private const string filename_body = "Enemies/gloop/prince-gloop-body";
         private const string filename_base = "Enemies/gloop/prince-gloop-base";
         private const string filename_eye = "Enemies/gloop/king-gloop-eye";
+
+        /// <summary>
+        /// This is both the minimum number of hit points it is possible for this boss to have
+        /// as well as the step-size for each additional hitpoint requested.
+        /// E.g., if you request this boss have "2" HP, then he will *really* get "2 x realHitPointMultiplier" HP
+        /// </summary>
+        private const int realHitPointMultiplier = 2;
+
+        /// <summary>
+        /// The scale of the pupil
+        /// </summary>
+        private const float scale_eyePupil = 0.9f;
+
+        /// <summary>
+        /// The max offset of the pupil
+        /// </summary>
+        private const float scale_eyeOffset = 60f;
+
+        private const float verticalOffsetHighlight = -30f;
+
+        private const float radiusMultiplier = 0.8f;
+        private const float outlineScale = 1.1f;
         #endregion
 
         #region Properties
         #endregion
 
         #region Fields
+        // Image rendering stuff
+        private Texture2D texture_body;
+        private Texture2D texture_base;
+        private Texture2D texture_eye;
+        private Texture2D texture_highlight;
+        private Texture2D texture_death;
+        private Vector2 center_body;
+        private Vector2 center_highlight;
+        private Vector2 center_eye;
+        private Vector2 offset_eye;
+        private Vector2 offset_highlight;
+        private Color color_eye;
         #endregion
 
         #region Constructor / Init
@@ -63,7 +97,48 @@ namespace Duologue.PlayObjects
             ColorPolarity startColorPolarity,
             int? hitPoints)
         {
-            throw new NotImplementedException();
+            Position = startPos;
+            Orientation = startOrientation;
+            ColorState = currentColorState;
+            ColorPolarity = startColorPolarity;
+            if (hitPoints == null || (int)hitPoints == 0)
+            {
+                hitPoints = 1;
+            }
+            StartHitPoints = (int)hitPoints * realHitPointMultiplier;
+            CurrentHitPoints = (int)hitPoints * realHitPointMultiplier;
+            //audio = ServiceLocator.GetService<AudioManager>();
+            LoadAndInitialize();
+        }
+
+        private void LoadAndInitialize()
+        {
+            texture_base = InstanceManager.AssetManager.LoadTexture2D(filename_base);
+            texture_body = InstanceManager.AssetManager.LoadTexture2D(filename_body);
+            texture_death = InstanceManager.AssetManager.LoadTexture2D(filename_gloopletDeath);
+            texture_eye = InstanceManager.AssetManager.LoadTexture2D(filename_eye);
+            texture_highlight = InstanceManager.AssetManager.LoadTexture2D(filename_gloopletHighlight);
+
+            center_body = new Vector2(
+                texture_base.Width / 2f, texture_base.Height / 2f);
+            center_eye = new Vector2(
+                texture_eye.Width / 2f, texture_eye.Height / 2f);
+            center_highlight = new Vector2(
+                texture_highlight.Width / 2f, texture_highlight.Height / 2f);
+
+            if (MWMathHelper.CoinToss())
+            {
+                color_eye = new Color(85, 145, 36);
+            }
+            else
+            {
+                color_eye = new Color(142, 109, 42);
+            }
+            offset_eye = Vector2.Zero;
+
+            Radius = RealSize.X / 2f * radiusMultiplier;
+            Initialized = true;
+            Alive = true;
         }
 
         public override String[] GetTextureFilenames()
@@ -104,7 +179,65 @@ namespace Duologue.PlayObjects
         #region Draw / Update
         public override void Draw(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            // Outline
+            InstanceManager.RenderSprite.Draw(
+                texture_body,
+                Position,
+                center_body,
+                null,
+                Color.DarkSlateGray,
+                0f,
+                outlineScale,
+                0f,
+                RenderSpriteBlendMode.AlphaBlend);
+
+            // Eye base
+            InstanceManager.RenderSprite.Draw(
+                texture_base,
+                Position,
+                center_body,
+                null,
+                Color.White,
+                0f,
+                1f,
+                0f,
+                RenderSpriteBlendMode.AlphaBlendTop);
+
+            // Pupil
+            InstanceManager.RenderSprite.Draw(
+                texture_eye,
+                Position + offset_eye,
+                center_eye,
+                null,
+                color_eye,
+                0f,
+                scale_eyePupil,
+                0f,
+                RenderSpriteBlendMode.AlphaBlendTop);
+
+            // Body
+            InstanceManager.RenderSprite.Draw(
+                texture_body,
+                Position,
+                center_body,
+                null,
+                Color.DarkSlateGray,
+                0f,
+                1f,
+                0f,
+                RenderSpriteBlendMode.AlphaBlendTop);
+
+            // Highlight
+            InstanceManager.RenderSprite.Draw(
+                texture_highlight,
+                Position + Vector2.UnitY * verticalOffsetHighlight,
+                center_highlight,
+                null,
+                Color.White,
+                0f,
+                1f,
+                0f,
+                RenderSpriteBlendMode.AlphaBlendTop);
         }
 
         public override void Update(GameTime gameTime)
