bb0b388ea5f316ae811fb53c2b4f6242afc3492a
Sam Hart <criswellious@gmail.com>
2009-05-19 16:44:06 -0400

    oops, that's what I meant

--- Duologue/PlayObjects/Enemy_Flambi.cs
+++ Duologue/PlayObjects/Enemy_Flambi.cs
@@ -430,9 +430,9 @@ namespace Duologue.PlayObjects
                     Position,
                     center_Flame[i],
                     null,
-                    new Color(flameColor, MathHelper.Lerp(alpha_MaxFlame, alpha_MinFlame, state_Flame[i])),
+                    new Color(flameColor, MathHelper.Lerp(alpha_MinFlame, alpha_MaxFlame, state_Flame[i])),
                     rotation_Flame[i],
-                    MathHelper.Lerp(scale_MaxFlame, scale_MinFlame, state_Flame[i]),
+                    MathHelper.Lerp(scale_MinFlame, scale_MaxFlame, state_Flame[i]),
                     0f,
                     RenderSpriteBlendMode.AddititiveTop);
             }
