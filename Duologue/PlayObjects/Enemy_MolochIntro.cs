4f33192e1b13df7117a660fb78256d15966b1dde
Sam Hart <criswellious@gmail.com>
2009-06-10 11:09:45 -0400

    add stubbed moloch intro to project

---
+++ Duologue/PlayObjects/Enemy_MolochIntro.cs
@@ -0,0 +1,88 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware;
+using Mimicware.Graphics;
+using Mimicware.Manager;
+// Duologue
+using Duologue.Audio;
+using Duologue.State;
+using Duologue.Screens;
+#endregion
+
+
+namespace Duologue.PlayObjects
+{
+    public class Enemy_MolochIntro : Enemy
+    {
+        #region Constants
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructor / Init
+        public override void Initialize(
+            Vector2 startPos, 
+            Vector2 startOrientation, 
+            ColorState currentColorState, 
+            ColorPolarity startColorPolarity, 
+            int? hitPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetTextureFilenames()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Motion overrides
+        public override bool StartOffset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UpdateOffset(PlayObject pobj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ApplyOffset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool TriggerHit(PlayObject pobj)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Draw / Update
+        public override void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
