708d1dd38046c664827572ae8334884e7417140e
Sam Hart <criswellious@gmail.com>
2009-07-01 15:07:23 -0400

    add stubbed out buy screen classes

---
+++ Duologue/Screens/BuyScreen.cs
@@ -0,0 +1,36 @@
+﻿#region File Description
+#endregion
+
+#region Using Statements
+// System
+using System;
+using System.Collections.Generic;
+// XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+// Mimicware
+using Mimicware.Manager;
+using Mimicware.Graphics;
+using Mimicware;
+using Mimicware.Fx;
+// Duologue
+using Duologue;
+using Duologue.Audio;
+using Duologue.State;
+using Duologue.Properties;
+using Duologue.Screens;
+using Duologue.PlayObjects;
+using Duologue.Waves;
+using Duologue.UI;
+#endregion
+
+namespace Duologue.Screens
+{
+    public class BuyScreen : DrawableGameComponent
+    {
+    }
+}
