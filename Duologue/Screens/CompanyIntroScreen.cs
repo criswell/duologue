772b898a3951513076c69970d5a78827196467b4
Sam Hart <criswellious@gmail.com>
2009-07-30 09:38:08 -0400

    meh, let's make that conditional

--- Duologue/Screens/CompanyIntroScreen.cs
+++ Duologue/Screens/CompanyIntroScreen.cs
@@ -426,11 +426,15 @@ namespace Duologue.Screens
             LocalInstanceManager.Spinner.TrackerColor = Color.SkyBlue;
             
             // Location and scale
+#if XBOX
             position_Loading = new Vector2(
                 InstanceManager.DefaultViewport.TitleSafeArea.Right - loadingSize.X - loadingPadding,
                 InstanceManager.DefaultViewport.TitleSafeArea.Bottom - loadingSize.Y - loadingPadding);
-                //(float)InstanceManager.DefaultViewport.Width * InstanceManager.TitleSafePercent - loadingSize.X,
-                //(float)InstanceManager.DefaultViewport.Height * InstanceManager.TitleSafePercent - loadingSize.Y);
+#else
+            position_Loading = new Vector2(
+                (float)InstanceManager.DefaultViewport.Width * InstanceManager.TitleSafePercent - loadingSize.X,
+                (float)InstanceManager.DefaultViewport.Height * InstanceManager.TitleSafePercent - loadingSize.Y);
+#endif
             LocalInstanceManager.Spinner.Position = position_Loading;
             LocalInstanceManager.Spinner.Scale = Vector2.One;
 
